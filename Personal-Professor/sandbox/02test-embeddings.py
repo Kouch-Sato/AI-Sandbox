79d56a1a56d7dc53e8a1db6b300aeb05d033d42c
Kouch-Sato <redkouchred@gmail.com>
2025-11-08 17:53:51 +0900

    text

--- Personal-Professor/sandbox/02test-embeddings.py
+++ Personal-Professor/sandbox/02test-embeddings.py
@@ -7,25 +7,25 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-base_text = "織田信長は1560年に桶狭間の戦いで今川義元を破った。"
+base_text = "今日は雨が降ってるな"
 
 base_emb = client.embeddings.create(
-  model = "text-embedding-3-small",
+  model = "text-embedding-3-large",
   input = base_text
 )
 base_vec = base_emb.data[0].embedding
 
 compare_texts = [
-  "こーちは28歳の男性です",
-  "おーい！お茶",
-  "今日はいい天気ですね。",
-  "徳川家康は1600年に関ヶ原の戦いで石田三成率いる西軍を倒した！",
-  "今川義元は、織田信長に桶狭間で敗れた。",
-  "織田信長って人は、1560年に今川義元を桶狭間で破ったんだよね。"
+  "本日の天気は雨です",
+  "傘を持っていかないと",
+  "天気予報を確認しよう",
+  "私は神だ！",
+  "株式会社Flamers",
+  "This is a pen."
 ]
 
 compare_embs = client.embeddings.create(
-  model = "text-embedding-3-small",
+  model = "text-embedding-3-large",
   input = compare_texts
 )
 compare_vecs = [e.embedding for e in compare_embs.data]
