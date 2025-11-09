596d8f62d236e8d1d35bd802166ba4b965bd5a00
Kouch-Sato <redkouchred@gmail.com>
2025-11-09 16:25:29 +0900

    copy

---
+++ Personal-Professor/sandbox/04test-rag.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+from numpy import dot
+from numpy.linalg import norm
+import chromadb
+from chromadb.utils import embedding_functions
+
+load_dotenv()
+embedding_model = "text-embedding-3-large"
+
+client = chromadb.PersistentClient(path="./chroma_db")
+openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+  api_key = os.getenv("OPENAI_API_KEY"),
+  model_name = embedding_model
+)
+
+collection = client.get_or_create_collection(
+  name = "test_collection",
+  embedding_function = openai_ef
+)
+
+texts = [
+    "私はオランダで生まれて、東京で育ちました",
+    "趣味はボードゲームとポーカーです。",
+    "AIやスタートアップの世界にワクワクするタイプです。",
+    "休日はカフェで読書することが多い。",
+    "猫を飼っていて、毎朝一緒に起きます。",
+    "なみしろさんという方と結婚しています"
+]
+
+collection.upsert(
+  documents = texts, 
+  ids = [f"doc_{i}" for i in range(len(texts))]
+)
+
+query_text = "どこで生まれたの？"
+results = collection.query(
+  query_texts = [query_text],
+  n_results = 6,
+)   
+
+for i in range(6):
+    print(results["documents"][0][i], results["distances"][0][i])
