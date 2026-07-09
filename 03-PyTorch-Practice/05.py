590313195c62d66aba7714b7949cee0586a1d64e
Kouch-Sato <redkouchred@gmail.com>
2026-07-09 13:48:22 +0900

    lerobot

--- 03-PyTorch-Practice/05.py
+++ 03-PyTorch-Practice/05.py
@@ -1,6 +1,9 @@
 # MNISTの前半
 # https://zenn.dev/kouch/articles/a28316bffbdba8
 
+# MNISTの後半
+
+
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -13,30 +16,3 @@ test_datasets  = datasets.MNIST(root="data", train=False, download=True, transfo
 train_loader = DataLoader(train_datasets, batch_size=64, shuffle=True, num_workers=0)
 test_loader  = DataLoader(test_datasets, batch_size=256, shuffle=False, num_workers=0)
 
-x, y = train_datasets[10]
-print(x) 
-
-# model = torch.nn.Sequential(
-#   torch.nn.Linear(in_features = 2, out_features = 16),
-#   torch.nn.ReLU(), #活性化関数
-#   torch.nn.Linear(in_features = 16, out_features = 1),
-# )
-
-# loss_fn = torch.nn.MSELoss()
-# optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
-
-# model.train()
-
-# for epoch in range(1000):
-# 	pred_y = model(x)
-# 	loss = loss_fn(pred_y, y)
-
-# 	optimizer.zero_grad()
-# 	loss.backward()
-# 	optimizer.step()
-
-# test_x = torch.tensor([[1.0, 4.0], [2.0, 4.0]])
-# test_y = model(test_x)
-# true_y = torch.tensor([[1.0 ** 2 + 4.0 ** 2 + 5], [2.0 ** 2 + 4.0 ** 2 + 5]])
-# print ("正解は", true_y)
-# print ("予測は", test_y)
