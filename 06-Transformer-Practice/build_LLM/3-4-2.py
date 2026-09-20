996d1e1297535332e1c9bc2e94295caeb40361c5
Kouch-Sato <redkouchred@gmail.com>
2026-09-20 19:06:06 +0900

    causal attention

--- 06-Transformer-Practice/build_LLM/3-4-2.py
+++ 06-Transformer-Practice/build_LLM/3-4-2.py
@@ -2,24 +2,28 @@ import torch
 import torch.nn as nn
 
 class SelfAttention_v1(nn.Module):
-    def __init__(self, d_in, d_out):
+    def __init__(self, d_in, d_out, bias=False):
         super().__init__()
-        self.w_query = torch.nn.Parameter(torch.rand(d_in, d_out))
-        self.w_key = torch.nn.Parameter(torch.rand(d_in, d_out))
-        self.w_value = torch.nn.Parameter(torch.rand(d_in, d_out))
+        self.w_query = nn.Linear(d_in, d_out, bias=bias)
+        self.w_key = nn.Linear(d_in, d_out, bias=bias)
+        self.w_value = nn.Linear(d_in, d_out, bias=bias)
 
     def forward(self, x):
-        keys = x @ self.w_key
-        queries = x @ self.w_query
-        values = x @ self.w_value
+        keys = self.w_key(x)
+        queries = self.w_query(x)
+        values = self.w_value(x)
 
         attention_scores = queries @ keys.T
+        context_length = attention_scores.shape[0]
+
+        mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        masked_attention_scores = attention_scores.masked_fill(mask.bool(), -torch.inf)
+        
         d_k = keys.shape[1]
-        attention_weights = torch.softmax(attention_scores / (d_k ** 0.5), dim=-1)
+        self.attention_weights = torch.softmax(masked_attention_scores / (d_k ** 0.5), dim=-1)
 
-        context_vectors = attention_weights @ values
+        context_vectors = self.attention_weights @ values
         return context_vectors
-
 
 inputs = torch.tensor(
   [[0.43, 0.15, 0.89], # Your     (x^1)
@@ -33,11 +37,10 @@ inputs = torch.tensor(
 d_in = inputs.shape[1]
 d_out = 2 
 
-torch.manual_seed(123)
+torch.manual_seed(789)
 
 self_attention = SelfAttention_v1(d_in, d_out)
 context_vectors = self_attention(inputs)
 
-print(self_attention)
+print(self_attention.attention_weights)
 print(context_vectors.shape)
-print(context_vectors)
