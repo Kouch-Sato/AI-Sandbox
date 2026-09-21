8ffca77261089796243a4991bbd4ae73f406779c
Kouch-Sato <redkouchred@gmail.com>
2026-09-21 20:06:02 +0900

    MultiHeadAttention

--- 06-Transformer-Practice/build_LLM/3-6-2.py
+++ 06-Transformer-Practice/build_LLM/3-6-2.py
@@ -1,23 +1,21 @@
 import torch
 import torch.nn as nn
 
-class MultiHeadAttentionWrapper(nn.Module):
+class MultiHeadAttention(nn.Module):
     def __init__(self, d_in, d_out, context_length, dropout, num_heads, bias=False):
         super().__init__()
-        self.heads = nn.ModuleList([
-            CausalAttention(d_in, d_out, context_length, dropout, bias)
-            for i in range(num_heads)
-        ])
 
-    def forward(self, x):
-        return torch.cat([head(x) for head in self.heads], dim=-1)
+        assert (d_out % num_heads == 0), "割り切れないぜ"
 
-class CausalAttention(nn.Module):
-    def __init__(self, d_in, d_out, context_length, dropout, bias=False):
-        super().__init__()
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+
         self.w_query = nn.Linear(d_in, d_out, bias=bias)
         self.w_key = nn.Linear(d_in, d_out, bias=bias)
         self.w_value = nn.Linear(d_in, d_out, bias=bias)
+
+        self.out_proj = nn.Linear(d_out, d_out)
         self.dropout = nn.Dropout(dropout)
         self.register_buffer(
             'mask',
@@ -31,14 +29,27 @@ class CausalAttention(nn.Module):
         queries = self.w_query(x)
         values = self.w_value(x)
 
-        attention_scores = queries @ keys.transpose(1, 2) # batchが追加されたから、0次元は変えずに、1次元と2次元だけ変えたい
+        keys = keys.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(batch_size, num_tokens, self.num_heads, self.head_dim)
 
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        attention_scores = queries @ keys.transpose(2, 3)
         attention_scores.masked_fill_(self.mask[:num_tokens, :num_tokens].bool(), -torch.inf)
         
         d_k = keys.shape[-1]
-        self.attention_weights = torch.softmax(attention_scores / (d_k ** 0.5), dim=-1)
+        attention_weights = torch.softmax(attention_scores / (d_k ** 0.5), dim=-1)
+        attention_weights = self.dropout(attention_weights)
 
-        context_vectors = self.attention_weights @ values
+        context_vectors = (attention_weights @ values).transpose(1, 2)
+        context_vectors = context_vectors.contiguous().view(
+            batch_size, num_tokens, self.d_out
+        )
+        context_vectors = self.out_proj(context_vectors)
+
         return context_vectors
 
 inputs = torch.tensor(
@@ -51,13 +62,13 @@ inputs = torch.tensor(
 )
 
 d_in = inputs.shape[1]
-d_out = 2 
+d_out = 2
 
 torch.manual_seed(123)
 
 batch = torch.stack((inputs, inputs), dim=0)
 context_length = batch.shape[1]
-multi_head_attention = MultiHeadAttentionWrapper(d_in, d_out, context_length, 0.0, num_heads=3)
+multi_head_attention = MultiHeadAttention(d_in, d_out, context_length, dropout=0.0, num_heads=2)
 context_vec = multi_head_attention.forward(batch)
 
 print(context_vec.shape)
